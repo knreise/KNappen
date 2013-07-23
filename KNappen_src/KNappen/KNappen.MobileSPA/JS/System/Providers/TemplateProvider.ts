ae0dab3d44b84dd32feab0e903a995ae798219f6
Tedd Hansen <tedd@konge.net>
2013-07-23 21:20:26 +0200

    Fix to template engine

--- KNappen_src/KNappen/KNappen.MobileSPA/JS/System/Providers/TemplateProvider.ts
+++ KNappen_src/KNappen/KNappen.MobileSPA/JS/System/Providers/TemplateProvider.ts
@@ -56,24 +56,24 @@ module System.Providers {
                         r = (<any>r)();
                     return r;
                 });
-                ret = ret.replace(/\$IF(\(![\s\S]*?)\$ENDIF/gm, function (fullMatch, match, offset) {
-                    var v = match.replace(/^\(!([^\)]+)\).*/, "$1");
+                ret = ret.replace(/\$IF(\(\![\s\S]*?)\$ENDIF/gm, function (fullMatch, match, offset) {
+                    var v = match.replace(/^\(\!([^\)]+)\)[\s\S]*/gm, "$1");
                     var r = replacement[v];
                     // If it is a method, execute it
                     if (typeof r === "function")
                         r = (<any>r)();
                     if (!r)
-                        return match.replace(/^\([^\)]+\)(.*)/, "$1");
+                        return match.replace(/^\([^\)]+\)([\s\S]*)/gm, "$1");
                     return "";
                 });
                 ret = ret.replace(/\$IF(\([\s\S]*?)\$ENDIF/gm, function (fullMatch, match, offset) {
-                    var v = match.replace(/^\(([^\)]+)\).*/, "$1");
+                    var v = match.replace(/^\(([^\)]+)\)[\s\S]*/gm, "$1");
                     var r = replacement[v];
                     // If it is a method, execute it
                     if (typeof r === "function")
                         r = (<any>r)();
                     if (r)
-                        return match.replace(/^\([^\)]+\)(.*)/, "$1");
+                        return match.replace(/^\([^\)]+\)([\s\S]*)/gm, "$1");
                     return "";
                 });
             }
