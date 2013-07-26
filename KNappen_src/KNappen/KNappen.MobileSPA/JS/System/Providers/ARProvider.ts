13d2bf66ed71d99e23d5840f9bc518ce6da91c34
Tedd Hansen <tedd@konge.net>
2013-07-26 18:50:26 +0200

    ARProvider is now disabled when we don't have AR (no more error messages that AR doesn't exist).

--- KNappen_src/KNappen/KNappen.MobileSPA/JS/System/Providers/ARProvider.ts
+++ KNappen_src/KNappen/KNappen.MobileSPA/JS/System/Providers/ARProvider.ts
@@ -19,6 +19,11 @@ module System.Providers {
 
         public PreInit() {
             log.debug("ARProvider", "PreInit()");
+            // If we don't have AR then no need to run this
+            if (!compatibilityInfo.hasAR)
+                return;
+
+
             this.wikitudeProvider = new System.Providers.WikitudeProvider();
             this.wikitudeProvider.PreInit();
 
@@ -37,6 +42,10 @@ module System.Providers {
           * @param {function} htmlDrawCallBack Function to call for HTML content of POI: function(poi: System.Models.PointOfInterestBase) { return "<h1>id: " + poi.id + "</h1>"; }
           */
         public resultToPoI(poi: System.Models.KnockoutObservablePointOfInterestBaseArray, htmlDrawCallback: { (poi: System.Models.PointOfInterestBase): string; }) {
+            // If we don't have AR then no need to run this
+            if (!compatibilityInfo.hasAR)
+                return;
+
             var count: number = 0;
             if (poi && poi())
                 count = poi().length;
@@ -52,6 +61,10 @@ module System.Providers {
           * @param {bool} state True=enable AR camera, False=disable AR camera
           */
         public enableAR(state: bool) {
+            // If we don't have AR then no need to run this
+            if (!compatibilityInfo.hasAR)
+                return;
+
             log.debug("ARProvider", "Setting AR camera state to: " + state);
             this._this.wikitudeProvider.enableAR(state);
         }
