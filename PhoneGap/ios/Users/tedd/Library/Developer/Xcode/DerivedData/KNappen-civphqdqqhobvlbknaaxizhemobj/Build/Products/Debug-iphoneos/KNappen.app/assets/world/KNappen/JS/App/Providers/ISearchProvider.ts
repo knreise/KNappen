de67dc4acd8dbc9eebd13e3b0d247c7cad4bc89b
Tedd Hansen <tedd@konge.net>
2013-07-28 14:01:40 +0200

    Removed unnecessary files

--- PhoneGap/ios/Users/tedd/Library/Developer/Xcode/DerivedData/KNappen-civphqdqqhobvlbknaaxizhemobj/Build/Products/Debug-iphoneos/KNappen.app/assets/world/KNappen/JS/App/Providers/ISearchProvider.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../_References.ts" />
-module App.Providers {
-    
-    export interface ISearchProvider {
-        search(searchCriteria: App.Models.SearchCriteria, successCallback: { (searchResult: App.Models.SearchResult): void; }, errorCallback: { (errorMessage: string): void; });
-    }
-}
